b7a102c3e7ea7237e5d3e1fac23e0975fd8cb18c
Mohamed ahmed <ma614351@gmail.com>
2024-12-08 21:05:15 +0100

    sqlite && scaling

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
-import { Server } from "socket.io";
+import dotenv from "dotenv"
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
+dotenv.config()
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
+  setupPrimary();
+} else {
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    adapter: createAdapter(),
   });
-});
-io.on("connection", (socket) => {
-  socket.on("hello", (arg) => {
-    console.log(arg); 
+
+  const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
   });
-});
-io.on("connection", (socket) => {
-  socket.emit("hello", "world");
-});
- 
 
-io.on("connection", (socket) => {
-  socket.join("some room")
-  io.to("room 1").emit("hello", "world")
-  io.except("room 1").emit('hello', "world")
-  socket.leave("room 1 ")
-})
+  await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
-});
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+  });
+
+  io.on("connection", async (socket) => {
+    socket.on("chat message", async (msg) => {
+      let result;
+      try {
+        result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+      } catch (e) {
+        return;
+      }
+      io.emit("chat message", msg, result.lastID);
+    });
+    if (!socket.recovered) {
+      try {
+        await db.each(
+          "SELECT id, content FROM messages WHERE id > ?",
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit("chat message", row.content, row.id);
+          }
+        );
+      } catch (e) {
+        console.log(error);
+      }
+    }
+  });
+
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
