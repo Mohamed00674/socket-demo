69d943beadc14a3d09fb244ade5e8f21ac6e12ed
Mohamed ahmed <ma614351@gmail.com>
2024-12-08 15:26:58 +0100

    handling disco

--- index.js
+++ index.js
@@ -6,18 +6,35 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server)
+const io = new Server(server);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname , "index.html"))
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-    console.log("a user connected");
-    
-} )
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+io.on("connection", (socket) => {
+  socket.on("hello", (arg) => {
+    console.log(arg); 
+  });
+});
+io.on("connection", (socket) => {
+  socket.emit("hello", "world");
+});
+ 
+
+io.on("connection", (socket) => {
+  socket.join("some room")
+  io.to("room 1").emit("hello", "world")
+  io.except("room 1").emit('hello', "world")
+  socket.leave("room 1 ")
+})
 
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
